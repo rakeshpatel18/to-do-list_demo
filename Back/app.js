e0c667c1f08813d0d59b1ffec5dc1eed91a3fa73
rakeshpatel18 <rakeshpatel13777@gmail.com>
2025-12-17 11:36:33 +0530

    url

--- Back/app.js
+++ Back/app.js
@@ -2,12 +2,21 @@ import express from "express";
 import cors from 'cors'
 import AllRoutes from './routes/AllRoutes.js'
 import { PORT } from "./config/config.js";
+import Path from 'path'
 
 let app = express();
+const root = Path.join(Path.resolve()+"/dist")
+app.use(express.static(root))
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
+
 app.use(AllRoutes);
+app.get("/{*splat}",(req ,res)=>{
+  res.sendFile("index.html", {root});  
+})
 
 app.listen(PORT,()=>{
     console.log("server running with port",PORT)
